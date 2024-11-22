7ba597e34923b0268cb7d57ec038ab04275d9ecd
Pedro Sousa <pedrohigor.dev@gmail.com>
2024-11-22 16:28:13 -0300

    chore: model loaders created

--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,8 @@ import { Router } from "express"
 
 const routes = new Router()
 
-routes.get('/', (request, response) => {
-  return response.status(200).json({
+routes.get('/', async (request, response) => {
+  return response.status(201).json({
     message: 'Server is running ...'
   })
 })
