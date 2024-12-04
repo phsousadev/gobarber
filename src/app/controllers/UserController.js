a706cc40543171402a62d94adf575ac940ab3775
Pedro Sousa <pedrohigor.dev@gmail.com>
2024-12-04 15:46:35 -0300

    chore: adding validation in request data

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,8 +1,19 @@
-import User from "../models/User"
-import bcrypt from 'bcrypt'
+import * as Yup from 'yup'
+
+import User from '../models/User'
 
 class UserController {
   async store(request, response) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string().required(),
+      password: Yup.string().required().min(6)
+    })
+
+    if (!(await schema.isValid(request.body))) return response.status(400).json({
+      message: 'Validation fails'
+    })
+
     const { name, email, password } = request.body
 
     const emailAlreadyExists = await User.findOne({
@@ -26,6 +37,23 @@ class UserController {
   }
 
   async update(request, response) {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string().email(),
+      oldPassword: Yup.string().min(6),
+      password: Yup.string()
+        .min(6)
+        .when('oldPassword', (oldPassword, field) => oldPassword ? field.required() : field
+        ),
+      confirmPassword: Yup.string().when('password', (password, field) =>
+        password ? field.required().oneOf([Yup.ref('password')]) : field
+      )
+    })
+
+    if (!(await schema.isValid(request.body))) return response.status(400).json({
+      message: 'Validation fails'
+    })
+
     const { old_password, email, password } = request.body
 
     const user = await User.findByPk(request.userId)
