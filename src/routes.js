62b811ff7b5c2fd92de222b976c415d716a0265c
Pedro Sousa <pedrohigor.dev@gmail.com>
2024-12-10 17:44:28 -0300

    chore: create files tbales

--- src/routes.js
+++ src/routes.js
@@ -3,24 +3,54 @@ import { Router } from "express"
 import multer from 'multer'
 import multerConfig from './config/multer'
 
+/**
+ * Controllers
+ */
 import UserController from "./app/controllers/UserController"
 import SessionController from "./app/controllers/SessionController"
+import FileController from "./app/controllers/FileController"
+
+/**
+ * Middlewares
+ */
 import authMiddleware from './app/middlewares/auth'
 
+/**
+ * Routes configs
+ */
 const routes = new Router()
 const upload = multer(multerConfig)
 
+/**
+ * Routes
+ * => Users
+ */
 routes.post('/users', UserController.store)
+
+/**
+ * Routes
+ * => Sessions
+ */
 routes.post('/sessions', SessionController.store)
 
+/**
+ * Alert
+ * Routes below this function require the user to be authenticated
+ */
 routes.use(authMiddleware)
 
+
+/**
+ * Routes
+ * => Users
+ * ==> Authenticated
+ */
 routes.put('/users', UserController.update)
 
-routes.post('/files', upload.single('file'), (request, response) => {
-  return response.json({
-    message: ' Uploading ...'
-  })
-})
+/**
+ * Routes
+ * => Files
+ */
+routes.post('/files', upload.single('file'), FileController.store)
 
 export default routes
