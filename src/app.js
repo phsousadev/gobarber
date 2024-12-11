a1b7d36e9ef75f77f22708bc8d193714c85f0681
Pedro Sousa <pedrohigor.dev@gmail.com>
2024-12-11 15:48:24 -0300

    feature: handling errors

--- src/app.js
+++ src/app.js
@@ -1,14 +1,25 @@
-import express from 'express'
+import express, { response } from 'express'
+import 'express-async-errors'
 import routes from './routes'
 import path, { dirname } from 'node:path'
+import * as Sentry from '@sentry/node'
+
+import Youch from 'youch'
+
+import sentryConfig from './config/sentry'
 
 import './database'
+import { request } from 'node:http'
 class App {
   constructor() {
     this.server = express()
 
+    Sentry.init(sentryConfig)
+
+    this.apm()
     this.middlewares()
     this.routes()
+    this.exceptionHandler()
   }
 
   middlewares() {
@@ -20,6 +31,18 @@ class App {
 
   routes() {
     this.server.use(routes)
+  }
+
+  apm() {
+    Sentry.setupExpressErrorHandler(this.server)
+  }
+
+  exceptionHandler() {
+    this.server.use(async (err, request, response, next) => {
+      const errors = await new Youch(err, request).toJSON()
+
+      return response.status(500).json(errors)
+    })
   }
 }
 
