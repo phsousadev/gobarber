a989f5db4546278b39751f764ed44ae9c5275ed4
Pedro Sousa <pedrohigor.dev@gmail.com>
2024-12-11 08:16:28 -0300

    chore: configure templates cancelation appointment

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -143,6 +143,11 @@ class AppointmentController {
             model: User,
             as: 'provider',
             attributes: ['name', 'email']
+          },
+          {
+            model: User,
+            as: 'user',
+            attributes: ['name']
           }
         ]
       }
@@ -167,7 +172,18 @@ class AppointmentController {
     await Mail.sendMail({
       to: `${appointment.provider.name} <${appointment.provider.email}>`,
       subject: 'Agendamento cancelado',
-      text: 'Você tem um novo cancelamento'
+      template: 'cancellation',
+      context: {
+        provider: appointment.provider.name,
+        user: appointment.user.name,
+        date: format(
+          appointment.date,
+          "'dia' dd 'de' MMMM', às' H:mm'h'",
+          {
+            locale: pt
+          }
+        )
+      }
     })
 
     return response.status(204).json(appointment)
